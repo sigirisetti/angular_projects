fc27c12d185ae5dc3b29c0331ee5820db850e124
Sigirisetty Surya Kiran <sigirisetti@gmail.com>
2022-12-14 17:39:00 +0800

    bar chart

--- ng-clickhouse-client/src/app/clickhouse/monitoring/memory/memory.component.ts
+++ ng-clickhouse-client/src/app/clickhouse/monitoring/memory/memory.component.ts
@@ -1,5 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
+
+import { ChartConfiguration, ChartData, ChartEvent, ChartType } from 'chart.js';
+import { BaseChartDirective } from 'ng2-charts';
+import DataLabelsPlugin from 'chartjs-plugin-datalabels';
 
 import { MonitoringService } from '../../monitoring.service';
 import { ChDbTableSize } from 'src/app/common/model/ch-db-table-size';
@@ -51,12 +55,71 @@ export class MemoryComponent {
     console.log(this.dbSelectionForm.value);
     this.monitoringService.dbTableSize(true, true)
       .subscribe((data: ChDbTableSize[]) => {
-        this.dataSource = data;
         console.log("Got data of length : " + data.length);
+        this.dataSource = data;
+        this.barChartData.labels = [];
+
+        let newLabels = [];
+        let newNumbers = [];
+        for(let r of data) {
+          newLabels.push(r.table);
+          newNumbers.push(r.size);
+        }
+        this.barChartData.labels = newLabels;
+        this.barChartData.datasets[0].data = newNumbers;
+        console.log(this.barChartData.datasets[0].data)
       });
   }
 
   updateName() {
     console.log("Button clicked")
   }
+
+  /**
+   * Charting code
+   */
+
+  @ViewChild(BaseChartDirective) chart: BaseChartDirective | undefined;
+
+  public barChartOptions: ChartConfiguration['options'] = {
+    responsive: true,
+    // We use these empty structures as placeholders for dynamic theming.
+    scales: {
+      x: {},
+      y: {
+        min: 10
+      }
+    },
+    plugins: {
+      legend: {
+        display: true,
+      },
+      datalabels: {
+        anchor: 'end',
+        align: 'end'
+      }
+    }
+  };
+  public barChartType: ChartType = 'bar';
+  public barChartPlugins = [
+    DataLabelsPlugin
+  ];
+
+  public barChartData: ChartData<'bar'> = {
+    labels: [],
+    datasets: [
+      {data: [], label: 'Table' }
+    ]
+  };
+
+  // events
+  public chartClicked({ event, active }: { event?: ChartEvent, active?: {}[] }): void {
+    console.log(event, active);
+  }
+
+  public chartHovered({ event, active }: { event?: ChartEvent, active?: {}[] }): void {
+    console.log(event, active);
+  }
+
+  public randomize(): void {}
 }
